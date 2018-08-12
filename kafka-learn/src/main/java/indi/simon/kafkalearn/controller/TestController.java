091d541778e55864771caefe2cc4e1a863e8e477
Simons <18317022181@163.com>
2018-08-12 18:23:00 +0800

    更新

--- kafka-learn/src/main/java/indi/simon/kafkalearn/controller/TestController.java
+++ kafka-learn/src/main/java/indi/simon/kafkalearn/controller/TestController.java
@@ -1,5 +1,7 @@
 package indi.simon.kafkalearn.controller;
 
+import indi.simon.kafkalearn.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/hello")
 public class TestController {
 
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("/kafka")
     @ResponseBody
     public String sayHello(@RequestParam String what){
-        return "hello";
+        return testService.sayHello();
     }
 
 
---
+++ kafka-learn/src/main/java/indi/simon/kafkalearn/service/TestService.java
@@ -0,0 +1,18 @@
+package indi.simon.kafkalearn.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date:
+ * Time:
+ */
+@Service
+public class TestService {
+
+    public String sayHello(){
+        return "hello";
+    }
+
+}
