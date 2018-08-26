a89be687492ed18553a997ddab02c38564a5f5ff
chenzhuo <simon.chen@ximalaya.com>
2018-08-26 10:35:51 +0800

    spark sql

--- spark-learn/src/main/scala/indi.simon.sparklearn/SparkSQLearn.scala
+++ spark-learn/src/main/scala/indi.simon.sparklearn/SparkSQLearn.scala
@@ -12,11 +12,17 @@ object SparkSQLearn {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder().master("local").appName("firstSparkSqlApp").getOrCreate();
+    val spark = SparkSession.builder().master("local").appName("firstSparkSqlApp").getOrCreate()
 
-    val sQLContext = spark.sqlContext;
+    val sQLContext = spark.sqlContext
 
+    val dataFrame = spark.read.format("json").load("hdfs://localhost:9000/testJson")
 
+    dataFrame.createOrReplaceTempView("data")
+
+    val propJson = spark.sql("select props from data limit 100")
+
+    propJson.write.format("text").save("hdfs://localhost:9000/output/")
 
 
 
