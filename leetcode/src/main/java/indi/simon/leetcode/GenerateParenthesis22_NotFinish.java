c7af8cc26516944d1efe3233831c8b34e8ff1350
chenzhuo <simon.chen@ximalaya.com>
2019-09-21 22:25:53 +0800

    更新

---
+++ leetcode/src/main/java/indi/simon/leetcode/GenerateParenthesis22_NotFinish.java
@@ -0,0 +1,26 @@
+package main.java.indi.simon.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date:
+ * Time:
+ */
+public class GenerateParenthesis22_NotFinish {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+
+}
---
+++ leetcode/src/main/java/indi/simon/leetcode/MergeKLists23.java
@@ -0,0 +1,23 @@
+package indi.simon.leetcode;
+
+import main.java.indi.simon.leetcode.TwoNumSumRevertList2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date:
+ * Time:
+ */
+public class MergeKLists23 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TwoNumSumRevertList2.ListNode mergeKLists(TwoNumSumRevertList2.ListNode[] lists) {
+
+
+        return null;
+    }
+
+}
