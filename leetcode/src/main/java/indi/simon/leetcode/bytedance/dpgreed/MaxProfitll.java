0356dc48ce7a59e537f8e8ba9a85d63e8d0be92d
chenzhuo <simon.chen@ximalaya.com>
2019-11-16 19:02:14 +0800

    更新

---
+++ leetcode/src/main/java/indi/simon/leetcode/bytedance/dpgreed/MaxProfitll.java
@@ -0,0 +1,76 @@
+package indi.simon.leetcode.bytedance.dpgreed;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date:
+ * Time:
+ */
+public class MaxProfitll {
+
+    private static int maxProfit = 0;
+    private static Integer minPrice = Integer.MAX_VALUE;
+
+    //备忘录,记录下
+    private static int[] buyInMem;
+
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 4, 5};
+        int maxProfit = maxProfit(prices);
+        System.out.println(maxProfit);
+    }
+
+    public static int maxProfit(int[] prices) {
+        buyInMem = new int[prices.length];
+        for (int index = 0; index < buyInMem.length; index++) {
+            buyInMem[index] = -1;
+        }
+        for (int i = 0; i < prices.length; i++) {
+            if (minPrice != null && prices[i] >= minPrice) {
+                continue;
+            }
+            minPrice = prices[i];
+            int result;
+            if (buyInMem[i] >= 0) {
+                result = buyInMem[i];
+            } else {
+                result = buyIn(i, prices);
+                buyInMem[i] = result;
+            }
+            if (result > maxProfit) {
+                maxProfit = result;
+            }
+        }
+        return maxProfit;
+    }
+
+    private static int buyIn(int i, int[] prices) {
+        int maxProfitThisRound = 0;
+        //j是卖出时刻
+        for (int j = i + 1; j < prices.length; j++) {
+            int maxProfitThisSell = 0;
+            if (prices[j] - prices[i] > 0) {
+                //穷举下一次买入，递归
+                int maxProfitNextBuyIn = 0;
+                for (int k = j + 1; k < prices.length; k++) {
+                    int profit;
+                    if (buyInMem[k] >= 0) {
+                        profit = buyInMem[k];
+                    } else {
+                        profit = buyIn(k, prices);
+                        buyInMem[k] = profit;
+                    }
+                    if (profit >= maxProfitNextBuyIn) {
+                        maxProfitNextBuyIn = profit;
+                    }
+                }
+                maxProfitThisSell += maxProfitNextBuyIn + prices[j] - prices[i];
+            }
+            if (maxProfitThisSell > maxProfitThisRound) {
+                maxProfitThisRound = maxProfitThisSell;
+            }
+        }
+        return maxProfitThisRound;
+    }
+
+}
---
+++ leetcode/src/main/java/indi/simon/leetcode/bytedance/listtree/MergeKLists.java
@@ -0,0 +1,78 @@
+package indi.simon.leetcode.bytedance.listtree;
+
+import indi.simon.leetcode.commonmodel.ListNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date:
+ * Time:
+ */
+public class MergeKLists {
+
+    public static void main(String[] args) {
+
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode11 = new ListNode(1);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode44 = new ListNode(4);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode6 = new ListNode(6);
+
+        listNode1.next = listNode4;
+        listNode4.next = listNode5;
+
+        listNode11.next = listNode3;
+        listNode3.next = listNode44;
+
+        listNode2.next = listNode6;
+
+        ListNode[] lists = new ListNode[3];
+        lists[0] = listNode1;
+        lists[1] = listNode11;
+        lists[2] = listNode2;
+
+        ListNode result = mergeKLists(lists);
+
+        System.out.println(result);
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode headNode = null;
+        ListNode currentNode = null;
+
+        while (true) {
+            int nullNum = 0;
+            int minValThisRound = Integer.MAX_VALUE;
+            int minListIndex = 0;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] == null) {
+                    nullNum++;
+                    continue;
+                }
+                if (lists[i].val < minValThisRound) {
+                    minListIndex = i;
+                    minValThisRound = lists[i].val;
+                }
+            }
+            //全为null了退出while循环
+            if (nullNum == lists.length) {
+                break;
+            }
+            if (headNode == null) {
+                headNode = lists[minListIndex];
+                currentNode = lists[minListIndex];
+            } else {
+                currentNode.next = lists[minListIndex];
+                currentNode = currentNode.next;
+            }
+            lists[minListIndex] = lists[minListIndex].next;
+        }
+
+        return headNode;
+    }
+
+}
