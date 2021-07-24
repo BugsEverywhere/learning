fbea473891d7a8da07461f1515893c766388478c
chenzhuo <zhiyue.cz@alibaba-inc.com>
2021-07-24 18:35:17 +0800

    加一些注释

--- nio-learn/src/main/java/indi/simon/learning/PlainNio2EchoServer.java
+++ nio-learn/src/main/java/indi/simon/learning/PlainNio2EchoServer.java
@@ -19,11 +19,14 @@ public class PlainNio2EchoServer {
         InetSocketAddress address = new InetSocketAddress(port);
         serverChannel.bind(address);
         final CountDownLatch latch = new CountDownLatch(1);
+        //只有AsynchronousServerSocketChannel可以监听accept事件，在这里就是调用accept方法，一旦获取了某个accept事件，并且建立连接了，会执行第二个参数CompletionHandler的相应方法
         serverChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>() {
             @Override
             public void completed(final AsynchronousSocketChannel channel, Object attachment) {
+                //在serverChannel中成功建立了连接，在这一步让其监听下一个accept事件
                 serverChannel.accept(null, this);
                 ByteBuffer buffer = ByteBuffer.allocate(100);
+                //从所建立的连接的channel读取内容到ByteBuffer中，读完之后会触发EchoCompletionHandler的相应方法的执行
                 channel.read(buffer, buffer, new EchoCompletionHandler(channel));
             }
 
@@ -55,14 +58,19 @@ public class PlainNio2EchoServer {
 
         @Override
         public void completed(Integer result, ByteBuffer buffer) {
+            //读完之后开始回写到client连接的channel
             buffer.flip();
+            //回写完执行所传入的CompletionHandler的相应方法
             channel.write(buffer, buffer, new CompletionHandler<Integer, ByteBuffer>() {
                 @Override
                 public void completed(Integer result, ByteBuffer buffer) {
                     if (buffer.hasRemaining()) {
+                        //如果buffer中还有残留，继续写完
                         channel.write(buffer, buffer, this);
                     } else {
+                        //如果buffer中没有残留了，
                         buffer.compact();
+                        //重新监听channel中的read事件，读完了就执行本EchoCompletionHandler实例中的相应方法
                         channel.read(buffer, buffer, EchoCompletionHandler.this);
                     }
                 }
@@ -81,6 +89,7 @@ public class PlainNio2EchoServer {
         @Override
         public void failed(Throwable exc, ByteBuffer attachment) {
             try {
+                //如果失败，就关闭该客户端channel
                 channel.close();
             } catch (IOException ignored) {
 
