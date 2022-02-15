98adb09de83dc5c0f8fba7267f06a34c9ee4e7a6
chenzhuo <zhiyue.cz@alibaba-inc.com>
2022-02-15 12:57:26 +0800

    update

--- leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220214/Quiz540.java
+++ leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220214/Quiz540.java
@@ -9,7 +9,7 @@ public class Quiz540 {
 
         Quiz540 quiz540 = new Quiz540();
 
-        int[] nums = new int[]{1, 3, 3, 4, 4, 5, 5, 7, 7, 10, 10, 11, 11};
+        int[] nums = new int[]{1, 1, 2, 2, 4, 4, 5, 5, 9};
 
         int res = quiz540.singleNonDuplicate(nums);
 
@@ -20,7 +20,11 @@ public class Quiz540 {
     public int singleNonDuplicate(int[] nums) {
         int resIndex = binaryBioSearch(nums, 0, nums.length - 2);
         if (resIndex == nums.length - 3) {
-            return nums[nums.length - 1];
+            if (nums[nums.length - 3] == nums[nums.length - 2]) {
+                return nums[nums.length - 1];
+            } else {
+                return nums[nums.length - 3];
+            }
         } else {
             return nums[resIndex];
         }
@@ -38,7 +42,8 @@ public class Quiz540 {
             return minIndex;
         }
 
-        int middlePairLeftIndex = (maxIndex - minIndex + 1) % 4 == 0 ? (maxIndex - minIndex + 1) / 2 - 2 : (maxIndex - minIndex) / 2;
+        int middlePairLeftIndex = (maxIndex - minIndex + 1) % 4 == 0 ? minIndex + (maxIndex - minIndex + 1) / 2 - 2 : minIndex + (maxIndex - minIndex) / 2;
+        //todo:这里多次出错，首先数组长度可被4整除和不能被4整除的情况下，求中间对的下标算错了；再然后忘记二分法通过下标截取数组，因而算出来的下表结果要从截取的minIndex加起
 
         if (originArr[middlePairLeftIndex] != originArr[middlePairLeftIndex + 1]) {
             //如果中间对，左元素不等于右元素，将包含他们的左数组继续递归
@@ -46,6 +51,7 @@ public class Quiz540 {
         } else {
             //如果中间对，左元素等于右元素，将不包含他们的右数组继续递归
             return binaryBioSearch(originArr, middlePairLeftIndex + 2, maxIndex);
+            //todo:此处粗心错了一次，第二个入参传了minIndex+2而不是middlePairLeftIndex + 2
         }
 
     }
