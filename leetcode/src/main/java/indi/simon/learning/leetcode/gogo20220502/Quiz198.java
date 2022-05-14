bb27476f22ec20cf7184b4891ebc78af6620b46d
chenzhuo <zhiyue.cz@alibaba-inc.com>
2022-05-14 17:33:03 +0800

    update

--- leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220502/Quiz198.java
+++ leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220502/Quiz198.java
@@ -8,7 +8,7 @@ public class Quiz198 {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 9, 3, 1};
         Quiz198 quiz198 = new Quiz198();
-        int res = quiz198.rob(nums);
+        int res = quiz198.robV2(nums);
         System.out.println(res);
 
     }
@@ -43,5 +43,21 @@ public class Quiz198 {
         mem[index] = Math.max(maxAfter, robThisOneMaxAmount);
         return mem[index];
     }
+
+    //自己尝试DP解法
+
+    public int robV2(int[] nums) {
+        int[][] dp = new int[nums.length + 1][2];
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i + 1][0] = dp[i][1] + nums[i];
+            dp[i + 1][1] = Math.max(dp[i][0], dp[i][1]);
+        }
+
+        return Math.max(dp[nums.length][0], dp[nums.length][1]);
+    }
+
 
 }
---
+++ leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220509/Quiz213_hinted.java
@@ -0,0 +1,39 @@
+package indi.simon.learning.leetcode.gogo20220509;
+
+/**
+ * @author chenzhuo(zhiyue)
+ */
+public class Quiz213_hinted {
+
+    public static void main(String[] args) {
+        Quiz213_hinted quiz213Hinted = new Quiz213_hinted();
+        int[] nums = new int[]{1,2,3,1};
+        int res = quiz213Hinted.rob(nums);
+        System.out.println(res);
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        return Math.max(robInternal(nums, 0, nums.length - 2), robInternal(nums, 1, nums.length - 1));
+    }
+
+    private int robInternal(int[] nums, int start, int end) {
+        int[][] dp = new int[end + 2][2];
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+
+        for (int i = start; i <= end; i++) {
+            dp[i + 1][0] = dp[i][1] + nums[i];
+            dp[i + 1][1] = Math.max(dp[i][0], dp[i][1]);
+        }
+
+        return Math.max(dp[end + 1][0], dp[end + 1][1]);
+    }
+}
