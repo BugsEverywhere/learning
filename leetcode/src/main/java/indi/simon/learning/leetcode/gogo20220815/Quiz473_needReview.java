9d5b054038e9980041b74084033cf088716c118e
chenzhuo <zhiyue.cz@alibaba-inc.com>
2022-10-01 13:32:28 +0800

    update

--- leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220815/Quiz473_needReview.java
+++ leetcode/src/main/java/indi/simon/learning/leetcode/gogo20220815/Quiz473_needReview.java
@@ -1,7 +1,6 @@
 package indi.simon.learning.leetcode.gogo20220815;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 /**
  * @author chenzhuo(zhiyue)
@@ -10,27 +9,51 @@ public class Quiz473_needReview {
 
     public static void main(String[] args) {
         Quiz473_needReview quiz473NeedReview = new Quiz473_needReview();
-        boolean res = quiz473NeedReview.makeSquareRecursive(new int[]{2, 2, 2, 2, 2, 6});
+        boolean res = quiz473NeedReview.makesquare(new int[]{1, 5, 3, 3, 3, 3, 4, 2});
         System.out.println(res);
     }
 
+    private Boolean[] statusMem;
+    private int per;
+
     public boolean makesquare(int[] matchsticks) {
         int sum = 0;
-        Map<Integer, Integer> stickCountMap = new HashMap<>();
-        for (int singleMatch : matchsticks) {
-            sum += singleMatch;
-            stickCountMap.merge(singleMatch, 1, Integer::sum);
+        int max = 0;
+        for (int match : matchsticks) {
+            sum += match;
+            max = Math.max(match, max);
         }
+
         if (sum % 4 != 0) {
             return false;
         }
-        int singleSide = sum / 4;
+        per = sum / 4;
+        Arrays.sort(matchsticks);
+        int statusCount = 1 << matchsticks.length;
+        statusMem = new Boolean[statusCount];
+        return dfs((1 << matchsticks.length) - 1, 0, matchsticks);
+    }
 
-        //dp[i][j]代表遍历到第i根火柴时是否可以拼出长度j
-        boolean[][] dp = new boolean[matchsticks.length][sum + 1];
-        dp[0][matchsticks[0]] = true;
-
-
+    private boolean dfs(int status, int sideSoFar, int[] matchsticks) {
+        if (status == 0) {
+            return true;
+        }
+        if (statusMem[status] != null) {
+            return statusMem[status];
+        }
+        statusMem[status] = false;
+        for (int i = 0; i < matchsticks.length; i++) {
+            if (sideSoFar + matchsticks[i] > per) {
+                break;
+            }
+            if (((status >> i) & 1) != 0) {
+                boolean res = dfs(status ^ (1 << i), (sideSoFar + matchsticks[i]) % per, matchsticks);
+                if (res) {
+                    statusMem[status] = true;
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
