09b6ab57801f01e5216c9be1d54c4979c15f301e
chenzhuo <zhiyue.cz@alibaba-inc.com>
2023-03-28 18:15:58 +0800

    update

---
+++ leetcode/src/main/java/indi/simon/learning/leetcode/gogo20230327/Quiz1638.java
@@ -0,0 +1,21 @@
+package indi.simon.learning.leetcode.gogo20230327;
+
+/**
+ * @author chenzhuo(zhiyue)
+ */
+public class Quiz1638 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int countSubstrings(String s, String t) {
+
+
+
+
+
+        return -1;
+    }
+
+}
