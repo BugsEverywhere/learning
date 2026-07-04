1dcb661709e74ab3b07336027aa16d32100c6369
18317022181@163.com <18317022181@163.com>
2026-07-04 13:46:41 +0800

    update

---
+++ leetcode/src/main/java/indi/simon/learning/复习/动态规划/回文/Quiz5_最长回文子串.java
@@ -0,0 +1,67 @@
+package indi.simon.learning.复习.动态规划.回文;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的 回文 子串。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ */
+public class Quiz5_最长回文子串 {
+
+
+    public String longestPalindrome(String s) {
+        // 特殊用例判断
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 更新记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+
+}
