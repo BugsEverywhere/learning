4309ba577da2ed358aec89aa7c15115b21b9c3a1
18317022181@163.com <18317022181@163.com>
2026-07-14 20:46:28 +0800

    update

--- leetcode/src/main/java/indi/simon/learning/复习/双指针_滑动窗口/Quiz76_最小覆盖子串.java
+++ leetcode/src/main/java/indi/simon/learning/复习/双指针_滑动窗口/Quiz76_最小覆盖子串.java
@@ -3,7 +3,46 @@ package indi.simon.learning.复习.双指针_滑动窗口;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 给定两个字符串 s 和 t，长度分别是 m 和 n，返回 s 中的 最短窗口 子串，使得该子串包含 t 中的每一个字符（包括重复字符）。如果没有这样的子串，返回空字符串 ""。
+ * <p>
+ * 测试用例保证答案唯一。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "ADOBECODEBANC", t = "ABC"
+ * 输出："BANC"
+ * 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+ * 示例 2：
+ * <p>
+ * 输入：s = "a", t = "a"
+ * 输出："a"
+ * 解释：整个字符串 s 是最小覆盖子串。
+ * 示例 3:
+ * <p>
+ * 输入: s = "a", t = "aa"
+ * 输出: ""
+ * 解释: t 中两个字符 'a' 均应包含在 s 的子串中，
+ * 因此没有符合条件的子字符串，返回空字符串。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * m == s.length
+ * n == t.length
+ * 1 <= m, n <= 105
+ * s 和 t 由英文字母组成
+ */
 public class Quiz76_最小覆盖子串 {
+
+
+    public static void main(String[] args) {
+        Quiz76_最小覆盖子串 quiz76_最小覆盖子串 = new Quiz76_最小覆盖子串();
+        String res = quiz76_最小覆盖子串.minWindow("cabwefgewcwaefgcf", "cae");
+        System.out.println(res);
+    }
 
     //用于记录t串中字符分布
     Map<Character, Integer> tMap = new HashMap<>();
@@ -11,51 +50,59 @@ public class Quiz76_最小覆盖子串 {
     Map<Character, Integer> sMap = new HashMap<>();
 
     public String minWindow(String s, String t) {
-        //先登记t串中字符
-        int tLen = t.length();
-        for (int i = 0; i < tLen; i++) {
-            char c = t.charAt(i);
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        char[] tArr = t.toCharArray();
+        for (char c : tArr) {
+            Integer cnt = tMap.getOrDefault(c, 0);
+            cnt++;
+            tMap.put(c, cnt);
         }
 
-        int l = 0, r = -1;
-        int len = Integer.MAX_VALUE;
-        //记录结果的左右边界
-        int ansL = -1, ansR = -1;
-        int sLen = s.length();
-        while (r < sLen) {
-            r++;
-            if (r < sLen && tMap.containsKey(s.charAt(r))) {
-                sMap.put(s.charAt(r), sMap.getOrDefault(s.charAt(r), 0) + 1);
+        int res = Integer.MAX_VALUE;
+        String resStr = "";
+
+        int i = 0;
+        int j = 0;
+
+        sMap.put(s.charAt(0), 1);
+
+        while (i < s.length() && j < s.length()) {
+            while (i <= j && check(sMap, tMap)) {
+                if (res > j - i + 1) {
+                    res = Math.min(res, j - i + 1);
+                    resStr = s.substring(i, j + 1);
+                }
+                //i左移
+                Integer iCharCnt = sMap.get(s.charAt(i));
+                if (iCharCnt == 1) {
+                    sMap.remove(s.charAt(i));
+                } else if (iCharCnt > 1) {
+                    sMap.put(s.charAt(i), iCharCnt - 1);
+                }
+                i++;
             }
-            while (check() && l <= r) {
-                if (r - l + 1 < len) {
-                    len = r - l + 1;
-                    ansL = l;
-                    ansR = l + len;
-                }
-                if (tMap.containsKey(s.charAt(l))) {
-                    sMap.put(s.charAt(l), sMap.getOrDefault(s.charAt(l), 0) - 1);
-                }
-                ++l;
+            //j左移
+            j++;
+            if (j < s.length()) {
+                Integer jCharCnt = sMap.getOrDefault(s.charAt(j), 0);
+                jCharCnt++;
+                sMap.put(s.charAt(j), jCharCnt);
             }
         }
-        return ansL == -1 ? "" : s.substring(ansL, ansR);
+
+        return resStr;
+
     }
 
-    /**
-     * 检查当前子串是否覆盖了t串
-     *
-     * @return
-     */
-    public boolean check() {
+    private boolean check(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+
         for (Map.Entry<Character, Integer> entry : tMap.entrySet()) {
-            Character key = entry.getKey();
-            Integer val = entry.getValue();
-            if (sMap.getOrDefault(key, 0) < val) {
+            char c = entry.getKey();
+            int cnt = entry.getValue();
+            if (!sMap.containsKey(c) || sMap.get(c) < cnt) {
                 return false;
             }
         }
+
         return true;
     }
 
