75a450671064851983bc1234727508dadf62572f
18317022181@163.com <18317022181@163.com>
2026-07-17 20:00:58 +0800

    update

--- leetcode/src/main/java/indi/simon/learning/复习/动态规划/回文/Quiz5_最长回文子串.java
+++ leetcode/src/main/java/indi/simon/learning/复习/动态规划/回文/Quiz5_最长回文子串.java
@@ -42,12 +42,12 @@ public class Quiz5_最长回文子串 {
         }
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
-                if (s.charAt(i) == s.charAt(j) && i + 1 <= j - 1 && dp[i + 1][j - 1]) {
+                if (s.charAt(i) == s.charAt(j) && i + 1 < j && dp[i + 1][j - 1]) {
                     //i和j不紧邻，那么他们构成一个回文串的前提是i+1与j-1也是回文串且i和j相等
                     dp[i][j] = true;
-                } else if (i + 1 == j) {
+                } else if (i + 1 == j && s.charAt(i) == s.charAt(j)) {
                     //i和j紧邻，那么他们相等的话就构成一个长度为2的回文串
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                    dp[i][j] = true;
                 }
 
                 // 更新记录回文长度和起始位置
--- leetcode/src/main/java/indi/simon/learning/复习/动态规划/回文/Quiz647_回文连续子串的个数.java
+++ leetcode/src/main/java/indi/simon/learning/复习/动态规划/回文/Quiz647_回文连续子串的个数.java
@@ -12,25 +12,25 @@ public class Quiz647_回文连续子串的个数 {
     public int countSubstrings(String s) {
         int n = s.length();
         //dp[i][j] = 1 表示 s[i~j] is 回文子串
-        int[][] dp = new int[n][n];
+        boolean[][] dp = new boolean[n][n];
 
         //对角线初始化
         for (int i = 0; i < n; i++) {
-            dp[i][i] = 1;
+            dp[i][i] = true;
         }
 
         int cnt = n;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
-                if (s.charAt(i) == s.charAt(j) && i + 1 <= j - 1 && dp[i + 1][j - 1] == 1) {
-                    //状态继承
-                    dp[i][j] = 1;
-                } else if (i + 1 == j) {
-                    //状态转移
-                    dp[i][j] = (s.charAt(i) == s.charAt(j) ? 1 : 0);
+                if (s.charAt(i) == s.charAt(j) && i + 1 < j && dp[i + 1][j - 1]) {
+                    //i和j不紧邻，那么他们构成一个回文串的前提是i+1与j-1也是回文串且i和j相等
+                    dp[i][j] = true;
+                } else if (i + 1 == j && s.charAt(i) == s.charAt(j)) {
+                    //i和j紧邻，那么他们相等的话就构成一个长度为2的回文串
+                    dp[i][j] = true;
                 }
                 //顺便记个数
-                cnt += dp[i][j];
+                cnt += dp[i][j] ? 1 : 0;
             }
         }
 
